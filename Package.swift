7ce108a13eac6b32852d5213a054ce9101dfa064
ladparis <laurent@dubertrand.fr>
2026-02-16 20:52:54 +0100

    Update deps for MLX 0.30.x + swift-transformers 1.1.x

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 
 import PackageDescription
 
@@ -11,8 +11,9 @@ let package = Package(
       targets: ["FluxSwift"])
   ],
   dependencies: [
-    .package(url: "https://github.com/ml-explore/mlx-swift", .upToNextMinor(from: "0.25.4")),
-    .package(url: "https://github.com/huggingface/swift-transformers",.upToNextMinor(from: "0.1.21")),
+    // Keep aligned with projects using recent MLX + swift-transformers
+    .package(url: "https://github.com/ml-explore/mlx-swift", .upToNextMinor(from: "0.30.3")),
+    .package(url: "https://github.com/huggingface/swift-transformers", .upToNextMinor(from: "1.1.6")),
     .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3")
   ],
   targets: [
@@ -24,7 +25,8 @@ let package = Package(
         .product(name: "MLXNN", package: "mlx-swift"),
         .product(name: "MLXOptimizers", package: "mlx-swift"),
         .product(name: "MLXRandom", package: "mlx-swift"),
-        .product(name: "Transformers", package: "swift-transformers"),
+        .product(name: "Hub", package: "swift-transformers"),
+        .product(name: "Tokenizers", package: "swift-transformers"),
         .product(name: "Logging", package: "swift-log"),
       ]
     ),
